8da34a4e9294927568717140f79eed2ddc61971a
Ilya Shapiro <ilya.m.shapiro@gmail.com>
2020-03-20 18:57:15 +0300

    added CarPart and menu selecting between parts and shops

---
+++ src/by/epam/entities/CarPart.java
@@ -0,0 +1,36 @@
+package by.epam.entities;
+
+public class CarPart {
+    private int id;
+    private String name;
+    private String description;
+    private String CarId;
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public String getCarId() { return CarId; }
+
+    public void setCarId(String carId) { CarId = carId; }
+
+    public String toStringFile(){
+        String temp = "-";
+        return id + temp + name + temp + description + temp + CarId;
+    }
+
+    public CarPart(int id, String name, String description, String carId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        CarId = carId;
+    }
+}
---
+++ src/by/epam/view/CarPartView.java
@@ -0,0 +1,124 @@
+package by.epam.view;
+
+import by.epam.entities.CarPart;
+import by.epam.service.CarPartService;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class CarPartView {
+
+    private final int menuReadAll = 1;
+    private final int menuCreate = 2;
+    private final int menuUpdate = 3;
+    private final int menuDelete = 4;
+
+    private CarPartService carPartService;
+
+    public CarPartView() { carPartService = new CarPartService();
+    }
+
+    public void Start() {
+        while (true) {
+            showMenu();
+            int check = readerInt();
+            switch (check) {
+                case (menuReadAll):
+                    readAllCarParts();
+                    break;
+                case (menuCreate):
+                    createCarPart();
+                    break;
+                case (menuUpdate):
+                    updateCarPart();
+                    break;
+                case (menuDelete):
+                    deleteCarPart();
+                default:
+                    System.out.println("Нет такого пункта");
+                    break;
+            }
+        }
+    }
+
+    private void deleteCarPart() {
+        System.out.println("Введите номер удаляемой запчасти:");
+        int id = readerInt();
+        System.out.println("Введите название удаляемой запчасти:");
+        String name = readerString();
+        carPartService.Delete(id,name);
+    }
+
+    private void updateCarPart() {
+        System.out.println("Введите номер изменяемой запчасти:");
+        int id = readerInt();
+        if (!readCarPart(id)) {
+            return;
+        }
+        try {
+            System.out.println("Введите новые данные запчасти в формате Имя-Описание-Автомобиль:");
+            String buffer = readerString();
+            StringTokenizer st = new StringTokenizer(buffer, "-");
+            String name = st.nextToken();
+            String description = st.nextToken();
+            String CarId = st.nextToken();
+            CarPart carPart = new CarPart(id, name, description,CarId);
+            carPartService.Update(id, carPart);
+        } catch (Exception e) {
+            System.out.println("Неверные данные");
+        }
+
+    }
+
+    private void createCarPart() {
+        System.out.println("Введите данные новой запчасти в формате Имя-Описание-Автомобиль:");
+        String buffer = readerString();
+        StringTokenizer st = new StringTokenizer(buffer, "-");
+        String name = st.nextToken();
+        String description = st.nextToken();
+        String CarId = st.nextToken();
+        carPartService.Create(new CarPart(0, name, description,CarId));
+    }
+
+    private boolean readCarPart(int id) {
+        try {
+            CarPart carPart = carPartService.ReadCarPart(id);
+            System.out.println(carPart.toStringFile());
+            return true;
+        } catch (Exception e) {
+            System.out.println(String.format("Ошибка %s", e.getMessage()));
+            return false;
+        }
+    }
+
+
+    private void showMenu() {
+        System.out.println(String.format("Введите %d  чтобы показать все запчасти", menuReadAll));
+        System.out.println(String.format("Введите %d  чтобы создать новую запчасть", menuCreate));
+        System.out.println(String.format("Введите %d  чтобы изменить данные запчасти", menuUpdate));
+        System.out.println(String.format("Введите %d  чтобы удалить запчасть", menuDelete));
+    }
+
+    private void readAllCarParts() {
+        ArrayList<CarPart> carParts = carPartService.ReadAll();
+        for (CarPart carPart : carParts
+        ) {
+            System.out.println(carPart.toStringFile());
+        }
+    }
+
+    public int readerInt() {
+        Scanner sc = new Scanner(System.in);
+        int tmp = sc.nextInt();
+        return tmp;
+    }
+
+    public String readerString() {
+        Scanner sc = new Scanner(System.in);
+        String tmp = sc.nextLine();
+        return tmp;
+    }
+}
+
+
